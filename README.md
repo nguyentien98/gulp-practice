f01620e4656b86a651492c35fd2b3b7343ec07b2
tiennguyen98 <nguyentien1098@gmail.com>
2018-04-20 15:46:16 +0700

    Update

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# gulp-practice
-Bài chuẩn bị 2.7
+# Bài tập Trainee Colombo 2018
+
+## Bài chuẩn bị 2.6
+
+Tìm hiểu và cài đặt gulp để compile sass sang css
+
+Người thực hiện: [ Tiến Nguyễn ](https://github.com/tiennguyen98)
+
+## Mục tiêu
+* Biết được gulp và cách dùng
+* Cấu trúc cơ bản của 1 project html với sass
+
+## Credit
+* SublimeText 3
+* Chrome
+* Git
+* [Gulp for Beginners](https://css-tricks.com/gulp-for-beginners)
+* [Gulp for Beginners](http://vth8.com/cau-truc-thu-muc-co-ban-cua-project)
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  .pipe(imagemin({
+      // Setting interlaced to true
+      interlaced: true
+    }))
+  .pipe(gulp.dest('dist/images'))
+});
+gulp.task('build', [`useref`, `images`], function (){
+  console.log('Building files');
+})
